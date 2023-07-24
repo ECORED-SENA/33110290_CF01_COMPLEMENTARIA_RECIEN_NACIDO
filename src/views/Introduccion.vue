7d41c528df19e2268c7a3c6e27e37722e62faac8
Andrea Botello <andrea.botellor@misena.edu.co>
2023-07-24 17:27:26 -0500

    Videos

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -24,8 +24,8 @@
 
     figure(data-aos="fade-down")
       .video
-        iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
-      figcaption Video.
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/JXOuBGGwhHg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+      figcaption Video. Marco general de la Ruta Integral de Atención en Salud a población Materno Perinatal (RIAMP) 
 
     
 </template>
--- src/views/Tema2.vue
+++ src/views/Tema2.vue
@@ -310,8 +310,8 @@
 
     figure(data-aos="fade-down")
       .video
-        iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
-      figcaption Video.
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/zBG8Rz2Isyg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+      figcaption Video. Intervenciones Colectivas
 
 
 </template>
